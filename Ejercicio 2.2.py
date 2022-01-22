6304da386a5c0009a1ae8fee8d3581a26ca0e3a3
Luis Pernia <pernia.luis@correo.unimet.edu.ve>
2022-01-22 19:56:43 -0400

    Ejercicios Prepas

---
+++ Ejercicio 2.2.py
@@ -0,0 +1,12 @@
+from typing import final
+
+
+bienvenida = "Bienvenido a la calculadora de Palindromos"
+bienvenida = bienvenida.center(50,"*")
+print(bienvenida)
+print("Palindromo significa que una cadena especifica de caracteres se leen igual de izquierda a derecha")
+numero = list(input("Por favor, ingrese una palabra o un numero:"))
+if numero[0]==numero[-1] and numero[1]==numero[-2] and numero[2]==numero[-3] and numero[3]==numero[-4]:
+    print("El numero o la palabra que ha ingresado es Palindromo.")
+else:
+    print("El numero o la palabra que ha ingresado no es Palindromo.")
