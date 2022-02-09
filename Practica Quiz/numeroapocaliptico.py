e9eadb8f339ac762c807f1a12ec3669abb172c9a
Luis Pernia <pernia.luis@correo.unimet.edu.ve>
2022-02-08 22:02:25 -0400

    Ejercicios Quiz 🍻�

---
+++ Practica Quiz/numeroapocaliptico.py
@@ -0,0 +1,20 @@
+from logging import getLogger
+
+
+bienvenida = "Bienvenidos a la calculadora de numeros apocalipticos"
+bienvenida = bienvenida.center(50,"*")
+print(bienvenida)
+aux=1
+while aux == 1:
+    number = input("Ingrese un numero:")
+
+
+
+
+
+
+
+
+    variable = input("Si quiere continuar presione 1:")
+    if variable == "1":
+        aux=2
